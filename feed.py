6a0007d855fa6253a1ce6c8ddf6a7caea609f490
Bogdan Grabinskij <bogdan.grabinskij@gmail.com>
2024-09-08 18:00:42 +0200

    Modify the output of categories

--- feed.py
+++ feed.py
@@ -6,8 +6,11 @@ import xml.etree.ElementTree as xml_tree
 def create_element(parent, tag, text=None, attrib=None):
     """Helper function to create an XML element with optional text and attributes."""
     elem = xml_tree.SubElement(parent, tag, attrib if attrib else {})
-    if text:
-        elem.text = text
+    if text is not None:
+        if 'text' in elem.tag:
+            elem.set('text', text)
+        else:
+            elem.text = text
     return elem
 
 
@@ -47,12 +50,9 @@ def main():
     create_element(channel_element, 'title', yaml_data.get('title', ''))
     create_element(channel_element, 'format', yaml_data.get('format', ''))
     create_element(channel_element, 'subtitle', yaml_data.get('subtitle', ''))
-    create_element(channel_element, 'itunes:author',
-                   yaml_data.get('author', ''))
-    create_element(channel_element, 'description',
-                   yaml_data.get('description', ''))
-    create_element(channel_element, 'itunes:image', None, {
-                   'href': link_prefix + yaml_data.get('image', '')})
+    create_element(channel_element, 'itunes:author', yaml_data.get('author', ''))
+    create_element(channel_element, 'description',yaml_data.get('description', ''))
+    create_element(channel_element, 'itunes:image', None, {'href': link_prefix + yaml_data.get('image', '')})
     create_element(channel_element, 'language', yaml_data.get('language', ''))
     create_element(channel_element, 'link', link_prefix)
     create_element(channel_element, 'itunes:email', yaml_data.get('email', ''))
@@ -62,11 +62,9 @@ def main():
         main_category = category.get('main', '')
         subcategory = category.get('subcategory', '')
         if main_category:
-            category_element = create_element(
-                channel_element, 'itunes:category', text=main_category)
+            category_element = create_element(channel_element, 'itunes:category', text=main_category)
             if subcategory:
-                create_element(category_element,
-                               'itunes:category', text=subcategory)
+                create_element(category_element,'itunes:category', text=subcategory)
         else:
             create_element(channel_element, 'itunes:category', text='')
     else:
@@ -76,12 +74,9 @@ def main():
         item_element = create_element(channel_element, 'item')
         create_element(item_element, 'guid', item.get('guid', ''))
         create_element(item_element, 'title', item.get('title', ''))
-        create_element(item_element, 'itunes:author',
-                       yaml_data.get('author', ''))
-        create_element(item_element, 'description',
-                       item.get('description', ''))
-        create_element(item_element, 'itunes:duration',
-                       item.get('duration', ''))
+        create_element(item_element, 'itunes:author', yaml_data.get('author', ''))
+        create_element(item_element, 'description',item.get('description', ''))
+        create_element(item_element, 'itunes:duration',item.get('duration', ''))
         create_element(item_element, 'pubDate', item.get('published', ''))
 
         create_element(item_element, 'enclosure', None, {
